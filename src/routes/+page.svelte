b495dd1391148a324a09ba4fdf697c8d74a4e061
Saroj <sarojregmi.official@gmail.com>
2025-03-15 20:14:26 +0545

    chore:made darkmode work

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,18 @@
+<script>
+    import Button from "$lib/components/ui/button/button.svelte";
+</script>
+
 <svelte:head>
     <title>Saroj Regmi | Home</title>
 </svelte:head>
 
-<div class="flex flex-col items-center justify-center h-screen w-screen">
-    <header>
-        This portfolio is under construction!
-    </header>
-    <p>
-        But you can check out my blogs.
-    </p>
+<div
+    class="flex flex-col items-center justify-center h-screen w-screen gap-2.5 px-10"
+>
+    <header>This portfolio is under construction!</header>
+    <p>But you can check out my blogs.</p>
 
-    <button>
-       Read Blogs  
-    </button>
+    <Button on:click={() => {}} class="mt-10">Read Blogs</Button>
 </div>
 
 <style>
