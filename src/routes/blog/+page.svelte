373c110e2cf0ef5d1f4f5e3a1e4af555b888e543
Saroj <sarojregmi.official@gmail.com>
2025-03-17 08:17:11 +0545

    chore: Manually adding the first blog post.

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,7 +1,24 @@
 <script lang="ts">
-    import { page } from "$app/state";
+    import { goto } from "$app/navigation";
+    import { Button } from "$lib/components/ui/button";
 </script>
 
 <svelte:head>
     <title>Saroj Regmi | Blog</title>
 </svelte:head>
+
+<div
+    class="flex flex-col items-center justify-center h-screen w-screen gap-2.5 px-10"
+>
+    <header class="text-center">The blog home page is not complete yet.</header>
+    <p>In the mean time there is only one blog that I have check it out.</p>
+
+    <Button
+        on:click={() => {
+            goto("/blog/wp-automation-taught-me-somethings-fun");
+        }}
+        class="mt-10"
+    >
+        Go Ahead.
+    </Button>
+</div>
---
+++ src/routes/blog/wp-automation-taught-me-somethings-fun/+page.svelte
@@ -0,0 +1,20 @@
+<div class="content w-full max-w-3xl flex gap-20 flex-col mb-50">
+    <div
+        class="top_header text-center flex flex-col gap-2.5 mt-50 items-center justify-center"
+    >
+        <header>Google Gemini! saved the day!!</header>
+        <p>
+            It was a crutial part of the job that was done by gemini to be more
+            specific Google AI studio. Which made me surprised and gave birth to
+            this article explaining what it was and how it helped me while other
+            tools were not reliable or turned their backs on me.
+        </p>
+    </div>
+
+    <div class="article-body flex flex-col gap-10">
+        <div class="chunk flex flex-col gap-2.5">
+            <h3>What and why??</h3>
+            <p>It was couple of days earlier</p>
+        </div>
+    </div>
+</div>
