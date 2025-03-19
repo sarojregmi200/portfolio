fbe285a56c66d157ea2e96180d36e13e6667ed3d
Saroj <sarojregmi.official@gmail.com>
2025-03-19 21:00:27 +0545

    chore: Managing the publishing and releases dir.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -16,17 +16,18 @@ if [ -z "$2" ]; then
 fi
 
 COMMIT_HASH=$1
-RELEASE_DIR=$2
+SITE_DIR=$2
+RELEASE_DIR="$SITE_DIR/releases/$COMMIT_HASH"
 SERVICE_NAME="portfolio"
 
-# Checking if the release exists in the directory.
-if [ ! -d "$RELEASE_DIR/" ]; then
+# Checking if the release exists in the release directory.
+if [ ! -d "$RELEASE_DIR" ]; then
     echo "Release not found in the release directory."
     exit 1
 fi
 
 # Promoting the current release to production. 
-ln -sf $RELEASE_DIR/$COMMIT_HASH $RELEASE_DIR/production
+ln -sf $RELEASE_DIR $SITE_DIR/current
 
 # Restarting the release.
 sudo systemctl restart ${SERVICE_NAME}
