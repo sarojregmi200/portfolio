74491205d11a650c26f54b3d7082d3d8d1ee5658
Saroj <sarojregmi.official@gmail.com>
2025-03-21 09:17:12 +0545

    wip: wp automation blog and setup

--- src/routes/blog/wp-automation-taught-me-somethings-fun/+page.svelte
+++ src/routes/blog/wp-automation-taught-me-somethings-fun/+page.svelte
@@ -1,20 +1,44 @@
 <div class="content w-full max-w-3xl flex gap-20 flex-col mb-50">
     <div
-        class="top_header text-center flex flex-col gap-2.5 mt-50 items-center justify-center"
+        class="top_header flex flex-col gap-2.5 mt-20 items-center justify-center"
     >
-        <header>Google Gemini! saved the day!!</header>
+        <header>I Automated 300+ Whatsapp Messages for a friend.</header>
         <p>
-            It was a crutial part of the job that was done by gemini to be more
-            specific Google AI studio. Which made me surprised and gave birth to
-            this article explaining what it was and how it helped me while other
-            tools were not reliable or turned their backs on me.
+            I mean automating couple of hundreds of messages is not that big of
+            a deal. But I was curious to see how it goes. Regardless it was a
+            decent learning experience. That taught me many things.
         </p>
     </div>
 
     <div class="article-body flex flex-col gap-10">
         <div class="chunk flex flex-col gap-2.5">
-            <h3>What and why??</h3>
-            <p>It was couple of days earlier</p>
+            <h3>Background, what and why?</h3>
+            <p>
+                I one day received a request from a friend asking me to help
+                them send 350+ messages individually in whatsapp. I without
+                having a second thought said yes and starting working on this
+                seemingly simple task. That is where the journey began.
+            </p>
+
+            <backquote>
+                I never planned to release this blog in the first place. I did
+                encounter some new and interesting finding which I wanted to
+                share. I tried sharing in Linkedin but quickly hit the
+                <code>max message</code> limit. So, after asking and thinking about
+                the pro plan. I came to conclusion setting a blog is a good idea.
+            </backquote>
+        </div>
+
+        <div class="chunk flex flex-col gap-2.5">
+            <h3>What and How i did it</h3>
+            <p>
+                I have actually open sourced the code on github. If you are
+                curious then you can check it here.
+
+                <a href="https://github.com/sarojregmi200/wp-automation">
+                    [Github Link]
+                </a>
+            </p>
         </div>
     </div>
 </div>
