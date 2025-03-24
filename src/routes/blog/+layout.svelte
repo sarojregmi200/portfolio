d083336eb0782fa9498f92aaeae4454fe9edd77d
Saroj Regmi <94213188+sarojregmi200@users.noreply.github.com>
2025-03-24 23:49:04 +0545

    chore: updated padding for mobile

--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -2,7 +2,7 @@
     let { children } = $props();
 </script>
 
-<article class="w-full p-10 h-screen">
+<article class="w-full py-10 px-5 sm:p-10 h-screen">
     <main class="flex justify-center items-center h-auto">
         {@render children()}
     </main>
