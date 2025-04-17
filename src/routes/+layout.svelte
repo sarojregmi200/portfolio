59e5ef19bfbbd15bd331a54d872081999888183e
Saroj <sarojregmi.official@gmail.com>
2025-04-17 08:51:39 +0545

    chore: added posthog web analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
     let { children } = $props();
     import "../app.css";
+
+    import posthog from "posthog-js";
+    import { browser } from "$app/environment";
+    import { onMount } from "svelte";
+
+    onMount(() => {
+        if (browser) {
+            posthog.init("phc_B2YTvdI7h8z7lQPQ1KYTx1qYxAiGUuSpS0KjmAmb9tJ", {
+                api_host: "https://us.i.posthog.com",
+                person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+            });
+        }
+    });
 </script>
 
 {@render children()}
