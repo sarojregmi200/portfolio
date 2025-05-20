36d6d41a26ba46ea3d2cf4f62adf53ab7fd26c5a
Saroj <sarojregmi.official@gmail.com>
2025-05-20 21:53:00 +0545

    feat: Made the blog statically build during deployment

--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -1,9 +1,9 @@
 <script>
-    let { children } = $props();
+	let { children } = $props();
 </script>
 
 <article class="w-full py-10 px-5 sm:p-10 h-screen">
-    <main class="flex justify-center items-center h-auto">
-        {@render children()}
-    </main>
+	<main class="flex justify-center items-center h-auto">
+		{@render children()}
+	</main>
 </article>
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { sveltekit } from "@sveltejs/kit/vite";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
 
 const PORT = import.meta.env?.PORT || 3000;
 
 export default defineConfig({
-    plugins: [tailwindcss(), sveltekit()],
-    server: {
-        port: PORT,
-    },
+	plugins: [tailwindcss(), sveltekit()],
+	server: {
+		port: PORT
+	}
 });
