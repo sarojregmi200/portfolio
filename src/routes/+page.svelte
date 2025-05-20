c374ade3b229f075660e78e1213ae85af0b28d76
Saroj <sarojregmi.official@gmail.com>
2025-05-20 23:35:46 +0545

    chore: corrected the mobile view

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,9 +7,11 @@
 	<title>Saroj Regmi | Home</title>
 </svelte:head>
 
-<div class="sm:p-20 p-5 space-y-5 flex flex-col gap-2.5 justify-center h-screen">
-	<sm class="text-xl dark:text-white text-neutral-500">Hey It's Me Saroj</sm>
-	<ul class="dark:text-white text-neutral-800 text-6xl flex flex-col gap-2.5">
+<div class="sm:p-20 p-2.5 flex flex-col gap-2.5 justify-center h-screen">
+	<sm class="sm:text-xl text-sm dark:text-gray-300 text-neutral-500">Hey It's Me Saroj</sm>
+	<ul
+		class="dark:text-white text-neutral-800 text-3xl sm:text-6xl flex flex-col sm:gap-2.5 gap-1 text-left"
+	>
 		<li>A developer for fun,</li>
 		<li>desinger by needs,</li>
 		<li>Linux and Vim user by heart.</li>
@@ -19,6 +21,6 @@
 		onclick={() => {
 			goto('/i automated whatsapp messages');
 		}}
-		className="self-end">Read a blog</Button
+		className="self-end mt-5">Read a blog</Button
 	>
 </div>
