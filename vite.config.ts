9968945e2f95a964d332db342686671d76752b22
Saroj <sarojregmi.official@gmail.com>
2025-07-22 22:46:09 +0545

    chore: corrected the ports config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+const PORT = import.meta.env?.PORT || 3000;
+
 export default defineConfig({
+  server: {
+    port: PORT
+  },
 	plugins: [tailwindcss(), sveltekit()]
 });
