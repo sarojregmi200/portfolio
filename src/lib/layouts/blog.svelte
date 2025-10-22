724a878d9a42a070e4d25d223a5396111dc84175
Saroj <sarojregmi.official@gmail.com>
2025-10-22 12:42:06 +0545

    feat: hero section styling done.

--- src/lib/layouts/blog.svelte
+++ src/lib/layouts/blog.svelte
@@ -1,31 +1,52 @@
 <script>
-	export let title, author, date, intro, cover_img;
+	export let title,
+		author = 'Saroj Regmi',
+		date,
+		intro,
+		cover_img,
+		keywords;
 </script>
 
-<article>
-	<section class="heading">
+<svelte:head>
+	<title>{title}</title>
+	<meta name="description" content={intro} />
+	<meta name="keywords" content={keywords} />
+	<meta name="author" content={author} />
+	<meta property="og:title" content={title} />
+
+	{#if cover_img}
+		<meta property="og:image" content={cover_img} />
+	{/if}
+
+	<meta property="og:title" content={title} />
+</svelte:head>
+
+<article class="mx-auto h-screen max-w-3xl px-5 py-10 sm:p-10">
+	<section class="heading flex h-3/4 flex-col items-center justify-center **:w-full">
 		{#if cover_img}
 			<img src={cover_img} alt={`cover image for ${title}`} />
 		{/if}
 
-		<h1>
+		<header>
 			{title}
-		</h1>
-		<p class="intro">
+		</header>
+
+		<p class="intro my-5">
 			{intro}
 		</p>
 
-		{#if author}
-			<p class="author">
-				{author}
-			</p>
-		{/if}
+		<div class="right ml-auto flex w-max! flex-col text-right">
+			{#if author}
+				<p class="author font-bold">
+					{author}
+				</p>
+			{/if}
 
-		<p class="date">
-			{date}
-		</p>
+			<p class="date text-xs">
+				- {date}
+			</p>
+		</div>
 	</section>
 
-	<slot>
-	</slot>
+	<slot></slot>
 </article>
