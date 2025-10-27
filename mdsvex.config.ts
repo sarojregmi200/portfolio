325aa92be55130752e636c8ec9fc97a4ca9a11b9
Saroj <sarojregmi.official@gmail.com>
2025-10-27 19:57:08 +0545

    feat: added heading link feature using rehype plugins.

--- mdsvex.config.ts
+++ mdsvex.config.ts
@@ -1,9 +1,16 @@
 import type { MdsvexOptions } from 'mdsvex';
 import { join } from 'node:path';
+import rehypeAutolinkHeadings, { type Options } from 'rehype-autolink-headings';
+import rehypeSlug from 'rehype-slug';
 
 const defineConfig = (config: MdsvexOptions) => config;
 
+const rehypeAutolinkHeadingsOptions: Options = {
+	behavior: 'wrap'
+};
+
 export default defineConfig({
+	rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, rehypeAutolinkHeadingsOptions]],
 	layout: {
 		_: join(import.meta.dirname, './src/lib/layouts/blog.svelte')
 	},
